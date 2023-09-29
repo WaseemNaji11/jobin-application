0182e0cd2a8230f5e4ee229872609054e4595f25
NawalAlBadarneh <144366884+NawalAlBadarneh@users.noreply.github.com>
2023-09-29 23:53:30 +0300

    forget password + register method

--- app/src/main/java/com/example/job_application/activities/LoginActivity.java
+++ app/src/main/java/com/example/job_application/activities/LoginActivity.java
@@ -15,6 +15,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 
+import java.lang.annotation.Target;
+
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText etEmail, etPassword;
@@ -24,13 +26,26 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+public void toForgetPassword(View view){
+        Intent intent =new
+        Intent(LoginActivity.this , ResetPasswordActivity.class);
+        startActivity(intent);
 
+
+}
     public void loginOnClick(View view) {
         String email = ((TextInputEditText) findViewById(R.id.etEmail)).getText().toString();
         String password = ((TextInputEditText) findViewById(R.id.etPassword)).getText().toString();
-        // TODO : Nawal =>  validate if the email is matches the regular email, if matches proceed to login, else not just return form this function
+        // TODO : Waseem =>  validate if the email is matches the regular email, if matches proceed to login, else not just return form this function
 
         login(email, password);
+
+
+    }
+    public void toRegisterOnClick(View view) {
+        Intent intent=new
+        Intent(LoginActivity.this , Register1Activity.class);
+        startActivity(intent);
 
 
     }
---
+++ app/src/main/java/com/example/job_application/activities/Register1Activity.java
@@ -0,0 +1,16 @@
+package com.example.job_application.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.job_application.R;
+
+public class Register1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register1);
+    }
+}
